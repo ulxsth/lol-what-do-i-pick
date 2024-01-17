2fd43028ea3a022d5668af61ca4dd1689d8e518e
ulxsth <ulxsth@gmail.com>
2024-01-17 11:37:06 +0900

    update: 現在選ばれているレーンを表示するように

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const app = new Vue({
   el: '#app',
   data: {
     characters: [],
+    selected: [],
+    selectedLanes: []
+  },
+  watch: {
+    selected: {
+      handler: function (newVal) {
+        console.log(newVal)
+        this.selectLane(newVal[newVal.length - 1]);
+      },
+      deep: true
+    }
   },
   created() {
     fetch('characters.csv')
@@ -9,5 +20,39 @@ const app = new Vue({
       .then((data) => {
         this.characters = Papa.parse(data, { header: true }).data
       })
+  },
+  methods: {
+    find(name) {
+      character = this.characters.find(character => character.name === name)
+      console.log(character)
+      return character
+    },
+    selectLane(name) {
+      const character = this.find(name)
+      if (!character) {
+        console.error('Character not found')
+        return
+      }
+
+      if (!this.isLaneSelected(character.lane1)) {
+        this.selectedLanes.push(character.lane1)
+        return
+      }
+
+      if (this.lane2 && !this.isLaneSelected(character.lane2)) {
+        this.selectedLanes.push(character.lane2)
+        return
+      }
+
+      if (this.lane3 && !this.isLaneSelected(character.lane3)) {
+        this.selectedLanes.push(character.lane3)
+        return
+      }
+
+      this.selectedLanes.push(character.lane1)
+    },
+    isLaneSelected(lane) {
+      return this.selectedLanes.includes(lane);
+    }
   },
 })
